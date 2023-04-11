027774f682d7561d9fd928bb8e00ba4a9811ff9a
vietdu91 <tran.emmanuel@outlook.fr>
2023-04-11 15:01:57 +0200

    cool raoul

--- Front/src/compenents/Menu.tsx
+++ Front/src/compenents/Menu.tsx
@@ -8,35 +8,38 @@ import { useNavigate } from "react-router-dom";
 // import useSound from 'use-sound';
 
 // COMPONENTS
-import Music from "./Music";
+import southparkSound from '../sounds/theme_song.mp3';
 
 // IMG
 import Hell from "../img/hell.jpg"
 import Town from "../img/south_park_town.jpg"
+import NoFriend from "../img/no_friend.jpg"
+import Metrosexual from "../img/metrosexual.png"
+import KennyHouse from "../img/kennyhouse.png"
 
 export default function App() {
-
+  const [hover, setHover] = React.useState(Town);
   const navigate = useNavigate();
 
-  function changeBackground(e) {
-    // e.target.style.background = 'red';
-    document.body.style.backgroundImage = 'url(../img/hell.jpg)'
-  }
-
-  function changeToNone(e) {
-    // e.target.style.background = 'none';
-  }
+  useEffect(() => {
+    const audio = new Audio(southparkSound);
+    audio.autoplay = true
+  }, [])
 
   return (
 		<div id="menu">
-      <Music />
+      <img id="bg-menu" src={hover} alt={'Hell'}></img>
   		<div id="menu-items">
-        {/* <div className="menu-item" onMouseEnter={changeBackground} onMouseLeave={changeToNone} onClick={() => navigate("/")}>Jeu</div> */}
-        <div className="menu-item" onClick={() => navigate("/")}>Jeu</div>
-        <div className="menu-item" onClick={() => navigate("/")}>Profil</div>
-        <div className="menu-item" onClick={() => navigate("/")}>Chat</div>
-        <div className="menu-item" onClick={() => navigate("/")}>On est qui</div>
-        <div className="menu-item" onClick={() => navigate("/score")}>Text Score</div>
+        <div className="menu-item" onClick={() => navigate("/")} onMouseEnter={() => {setHover(Hell);}}
+        onMouseLeave={() => {setHover(Town);}}>Jeu</div>
+        <div className="menu-item" onClick={() => navigate("/")} onMouseEnter={() => {setHover(KennyHouse);}}
+        onMouseLeave={() => {setHover(Town);}}>Profil</div>
+        <div className="menu-item" onClick={() => navigate("/")} onMouseEnter={() => {setHover(NoFriend);}}
+        onMouseLeave={() => {setHover(Town);}}>Chat</div>
+        <div className="menu-item" onClick={() => navigate("/")} onMouseEnter={() => {setHover(Metrosexual);}}
+        onMouseLeave={() => {setHover(Town);}}>On est qui</div>
+        <div className="menu-item" onClick={() => navigate("/score")} onMouseEnter={() => {setHover(Hell);}}
+        onMouseLeave={() => {setHover(Town);}}>Text Score</div>
       </div>
 		</div>
   )
