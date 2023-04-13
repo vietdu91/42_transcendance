73c12d5218d5e25a3c02565be9b6fe245ce38fc5
vietdu91 <tran.emmanuel@outlook.fr>
2023-04-13 23:08:32 +0200

    credits gay done

--- Front/src/compenents/Credits/Credits.tsx
+++ Front/src/compenents/Credits/Credits.tsx
@@ -2,20 +2,46 @@ import React from 'react'
 
 import './Credits.css'
 
-import Metrosexual from "../../img/metrosexual.png"
+import Metrosexual from "../../img/video/kyle_gets_a_queer_eye_makeover.mp4"
+
+function Cercle() {
+
+	return (
+			<div id="boutons">
+				<div className="Container">
+					<div id="creator">
+						<div className="round round-button1">
+							<div className="inner"></div>
+						</div>
+						<p>emtran</p>
+					</div>
+					<div id="creator">
+						<div className="round round-button2"></div>
+						<p>qujacob</p>
+					</div>
+					<div id="creator">
+						<div className="round round-button3"></div>
+						<p>jkaruk</p>
+					</div>
+					<div id="creator">
+						<div className="round round-button4"></div>
+						<p>dyoula</p>
+					</div>
+					<div id="creator">
+						<div className="round round-button5"></div>
+						<p>benmoham</p>
+					</div>
+				</div>
+			</div>
+	)
+}
 
 export default function Credits() {
 
 	return (
 		<div id="page_credits">
-      		<img id="bg" src={Metrosexual} alt={'metrosexual'}></img>
-			<div id="boutons">
-				<button className="round-button1"></button>
-				<button className="round-button2"></button>
-				<button className="round-button3"></button>
-				<button className="round-button4"></button>
-				<button className="round-button5"></button>
-			</div>
+      		<video autoPlay muted loop id="bg" src={Metrosexual}></video>
+			<Cercle />
 		</div>
 	)
 }
--- Front/src/react-app-env.d.ts
+++ Front/src/react-app-env.d.ts
@@ -12,3 +12,8 @@ declare module "*.jpg" {
 	const value: any;
 	export default value;
 }
+
+declare module '*.mp4' {
+	const src: string;
+	export default src;
+}
