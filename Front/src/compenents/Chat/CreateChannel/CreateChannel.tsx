6bcf5aa681e89a5ca75a2deb5256480cfbbe030e
QnYosa <dimitriyoula@gmail.com>
2023-08-29 14:55:52 +0200

    popup incoming

---
+++ Front/src/compenents/Chat/CreateChannel/CreateChannel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CreateChannel.css';
+
+const CreateChannel = () => {
+	return (
+	<div className="channel-creation-container">
+		<h1>kiki le raciste </h1>
+	</div>
+)}
+
+export default CreateChannel;
