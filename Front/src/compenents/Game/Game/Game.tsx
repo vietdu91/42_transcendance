0e99c38d675af1c007ef608f58954254a6e31320
Quentin JACOB <qujacob@made-f0Dr6s7.clusters.42paris.fr>
2023-09-20 05:59:59 +0200

    userId corrige jeu

--- Front/src/compenents/Game/Game/Game.tsx
+++ Front/src/compenents/Game/Game/Game.tsx
@@ -238,8 +238,7 @@ export default function Game(): JSX.Element {
 				},
 			}
 			game.current = updatedGame;
-			console.log(response.userId);
-			// setUserId(response.userId);
+			setUserId(response.userId);
 			setCharLeft(game.current.charLeft);
 			setCharRight(game.current.charRight);
 			setNameLeft(response.game.nameLeft);
